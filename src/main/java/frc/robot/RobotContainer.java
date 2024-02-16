ebb7eed08366820d2cebcab702359f56b0c484d5
Theinatorinator <Logan.mamanakis@gmail.com>
2024-02-15 19:47:15 -0600

    fix HAED~1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,12 +11,6 @@ import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.Shooting.ShootNote;
 import frc.robot.commands.drive.DefaultDrive;
 import frc.robot.subsystems.drivebase.DrivebaseSubsystem;
 import frc.robot.subsystems.drivebase.GyroIO;
@@ -127,15 +121,13 @@ public class RobotContainer {
         .a()
         .debounce(0.2)
         .onTrue(
-            new InstantCommand(
-                () -> flywheelSubsystem.setBothFlywheelSpeeds(flywheelSpeedInput.get())));
+            new InstantCommand(() -> shooterSubsystem.setFlywheelSpeeds(flywheelSpeedInput.get())));
 
     operatorController
         .b()
         .debounce(0.2)
         .onTrue(
-            new InstantCommand(
-                () -> flywheelSubsystem.setBothFlywheelSpeeds(flywheelSpeedInput.get())));
+            new InstantCommand(() -> shooterSubsystem.setFlywheelSpeeds(flywheelSpeedInput.get())));
 
     driverController.start().debounce(0.2).onTrue(drivebaseSubsystem.getZeroGyroCommand());
     driverController.x().debounce(0.2).onTrue(drivebaseSubsystem.getZeroPoseCommand());
